bfa66a1727a6f71c8fcd3c27cc2cb5e6d8ccbae9
Santiago Trini <santiagotrini@gmail.com>
2020-07-06 14:17:35 -0300

    termina UI

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// cuando termine de cargar el documento poblamos la tabla
+document.addEventListener("DOMContentLoaded", function() {
+  fetchData();
+});
+
+// esta funcion carga la tabla apenas carga la pagina
+// fetch() es el método nativo de los navegadores para hacer peticiones AJAX
+// se usa con promesas ya que es asíncrono
+function fetchData() {
+  fetch('https://hello-database.herokuapp.com/api/users')
+    .then(res => res.json())
+    .then(json => {
+      populateTable(json);
+    });
+}
+
+// esta funcion carga los usuarios en la tabla
+// crea una fila en la tabla por cada elemento en el array json
+function populateTable(json) {
+  for (let user of json) {
+    // borramos las propiedades que no queremos en la tabla
+    delete user._id;
+    delete user.__v;
+    // agregamos el cumpleaños para la ultima columna
+    let now = new Date();
+    user.age = now.getFullYear() - new Date(user.birthday).getFullYear();
+    let birthday = new Date(user.birthday);
+    user.birthday = birthday.toLocaleDateString('es-AR');
+    // creamos una fila
+    let row  = document.createElement('tr');
+    for (let key in user) {
+      // agregamos una columna por cada propiedad
+      let col = document.createElement('td');
+      col.innerHTML = user[key];
+      row.append(col);
+    }
+    // agregamos la fila al tbody y repetimos
+    document.getElementById('tbody').append(row);
+  }
+}
+
+// esta funcion carga un solo usuario en la tabla
+function fetchUser() {
+  let id = document.getElementById('userID').value;
+  // si el id no es un numero terminamos
+  if (isNaN(id)) return;
+  fetch(`https://hello-database.herokuapp.com/api/user/${id}`)
+    .then(res => res.json())
+    .then(user => {
+      let json = [user];  // populateTable usa un array de json como argumento
+      // si tenemos un usuario lo cargamos en la tabla
+      if (json[0] !== null) {
+        // reemplazamos el tbody por uno nuevo con el usuario que encontramos
+        let oldTBody = document.getElementById('tbody');
+        let newTBody = document.createElement('tbody');
+        newTBody.id = 'tbody';
+        oldTBody.replaceWith(newTBody);
+        // cargamos la fila
+        populateTable(json);
+        // actualizamos la UI
+        document.getElementById('table').hidden = false;
+        document.getElementById('nores').innerHTML = '';
+      } else {
+        // si no hay usuario con ese ID mostramos un mensaje
+        document.getElementById('table').hidden = true;
+        document.getElementById('nores').innerHTML = 'No hay resultados';
+      }
+    });
+}
